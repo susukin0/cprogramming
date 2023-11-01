0e6c764e4e232af14cc6afedc18ae5aebce24582
susukin0 <70662829+susukin0@users.noreply.github.com>
2023-11-01 11:24:04 +0300

    Add files via upload

---
+++ for3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+char ch;
+
+int main(){
+	
+	printf("lower case alphabets\n");
+	for(ch = 'a'; ch <= 'z'; ch++){
+		
+		printf("%d %c\n", ch, ch);
+	}
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
