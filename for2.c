3ffe8f8601b0e174d0129fd4b2706f839809a87b
susukin0 <70662829+susukin0@users.noreply.github.com>
2023-11-01 11:19:45 +0300

    Add files via upload

---
+++ for2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int n;
+int i;
+
+
+int main(){
+	
+	printf("enter a number: ");
+	scanf("%d", &n);
+	
+	i = n;
+	while(i>0){
+		
+		printf("%d", i);
+		i--;
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
