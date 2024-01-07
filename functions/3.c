892e48e0d7d588b817544dd43bd2c34a6ebf49dc
susukin0 <70662829+susukin0@users.noreply.github.com>
2024-01-07 22:16:17 +0300

    Update 3.c

--- functions/3.c
+++ functions/3.c
@@ -2,8 +2,8 @@
 int main(){
     int min,b,flag;
     scanf("%d %d", &min, &b);
-    printf("prime numbers betweeen %d and %d are:\n", min, b);
-    while(min<b){
+    printf("prime numbers between %d and %d are:\n", min, b);
+    while(min<b){ // asal sayi loopu, eger bolunecek sayinin bir boleni varsa flag 1dir ve print edilir. 
         flag=0;
         for(int divide=2; divide<min; ++divide){
             if(min % divide == 0){
